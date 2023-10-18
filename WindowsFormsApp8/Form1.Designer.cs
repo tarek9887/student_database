eac94ee03771b3559ef5f200c9298f1a777a0497
tarek9887 <139941002+tarek9887@users.noreply.github.com>
2023-10-18 19:22:16 +0600

    Add project files.

---
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.TextBox();
+            this.ID = new System.Windows.Forms.TextBox();
+            this.department = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(152, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Bright", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(152, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Matric ID";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Bright", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(155, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Department";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Lucida Bright", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(234, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Cyan;
+            this.button2.Font = new System.Drawing.Font("Lucida Bright", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(465, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Show";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // name
+            // 
+            this.name.Font = new System.Drawing.Font("Lucida Bright", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(404, 59);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(296, 39);
+            this.name.TabIndex = 5;
+            this.name.TextChanged += new System.EventHandler(this.name_TextChanged);
+            // 
+            // ID
+            // 
+            this.ID.Font = new System.Drawing.Font("Lucida Bright", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ID.Location = new System.Drawing.Point(404, 134);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(296, 39);
+            this.ID.TabIndex = 6;
+            this.ID.TextChanged += new System.EventHandler(this.ID_TextChanged);
+            // 
+            // department
+            // 
+            this.department.Font = new System.Drawing.Font("Lucida Bright", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.department.Location = new System.Drawing.Point(404, 206);
+            this.department.Name = "department";
+            this.department.Size = new System.Drawing.Size(296, 39);
+            this.department.TabIndex = 7;
+            this.department.TextChanged += new System.EventHandler(this.department_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(158, 345);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(507, 159);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(833, 510);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.department);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox ID;
+        private System.Windows.Forms.TextBox department;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApp8/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Collections.Specialized.BitVector32;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void name_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void department_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //save
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var connectionString = "Data Source=DESKTOP-RJLLT5B\\SQLEXPRESS03;Initial Catalog=employee;Integrated Security=True";
+
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+
+                var insertQuery = "insert into tarek values(@Studentname,@MatricID,@department)";
+
+                SqlCommand sqlCommand = new SqlCommand(insertQuery, sqlConnection);
+                sqlCommand.Parameters.AddWithValue("@Studentname", name.Text);
+                sqlCommand.Parameters.AddWithValue("@MatricID", ID.Text);
+                sqlCommand.Parameters.AddWithValue("@department", department.Text);
+
+               sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+
+               MessageBox.Show("Data Inserted Succesfully!!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Somthing went wrong");
+            }
+
+
+}
+//show
+private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var connectionString = "Data Source=DESKTOP-RJLLT5B\\SQLEXPRESS03;Initial Catalog=employee;Integrated Security=True";
+
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+
+                string readCommand = "";
+
+                if (ID.Text.ToString() == "")
+                {
+                    readCommand = "select * from tarek";
+                }
+                else
+                {
+                    readCommand = "select * from tarek where matricId=@matricId";
+                }
+
+                SqlCommand sqlCommand = new SqlCommand(readCommand, sqlConnection);
+                sqlCommand.Parameters.AddWithValue("@matricId", ID.Text);
+
+
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                DataTable dataTable = new DataTable();
+
+                sqlDataAdapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error");
+            }
+
+
+            }
+        }
+}
